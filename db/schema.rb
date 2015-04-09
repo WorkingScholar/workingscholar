364ec14b2cf6bfd1feccf124e0201d96b1e38dee
0xazure <stuart.crust@gmail.com>
2015-04-08 21:20:53 -0400

    Add School model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150407022938) do
+ActiveRecord::Schema.define(version: 20150409011258) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -28,5 +28,12 @@ ActiveRecord::Schema.define(version: 20150407022938) do
 
   add_index "accounts", ["email"], name: "index_accounts_on_email", unique: true, using: :btree
   add_index "accounts", ["reset_password_token"], name: "index_accounts_on_reset_password_token", unique: true, using: :btree
+
+  create_table "schools", force: :cascade do |t|
+    t.string   "name"
+    t.integer  "students_count", default: 0
+    t.datetime "created_at",                 null: false
+    t.datetime "updated_at",                 null: false
+  end
 
 end
