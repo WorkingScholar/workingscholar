8d4da7ce4e6b0795f327cfe8e3c841fce2a07120
Sheret Ross <sheretr@gmail.com>
2015-04-11 16:57:49 -0400

    add FriendlyId for employer

--- app/controllers/employers_controller.rb
+++ app/controllers/employers_controller.rb
@@ -1,25 +1,30 @@
 class EmployersController < ApplicationController
   skip_before_action :authenticate_account!, only: [:index]
 
+  before_action :set_employer, only: [:show, :edit, :update]
+
   def index
     @employers = Employer.all
   end
 
   def show
-    @employer = Employer.find(params[:id])
   end
 
   def edit
-    @employer = Employer.find(params[:id])
   end
 
   def update
-    @employer = Employer.find(params[:id])
     if @employer.update_attributes(employer_params)
       redirect_to employers_path, notice: "Succesfully updated your profile"
     else
       render "edit"
     end
+  end
+
+  private
+
+  def set_employer
+    @employer = Employer.friendly.find(params[:id])
   end
 
   def employer_params
