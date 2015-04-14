1dfd1930cb0f4855840ca022c0bc455a662a6dd7
0xazure <stuart.crust@gmail.com>
2015-04-14 18:48:48 -0400

    Implement ResumeEntry controller

--- app/models/resume_entry.rb
+++ app/models/resume_entry.rb
@@ -1,4 +1,5 @@
 class ResumeEntry < ActiveRecord::Base
+  validates :student, presence: true
   validates :job_title, presence: true
   validates :employer_name, presence: true
   validates :description, presence: true
