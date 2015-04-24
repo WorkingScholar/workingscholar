c1c99076c336a9471d98c0a013dcf890ece792c3
0xazure <stuart.crust@gmail.com>
2015-04-23 21:15:33 -0400

    Implement linking Profile to Account

--- app/models/account.rb
+++ app/models/account.rb
@@ -9,6 +9,11 @@ class Account < ActiveRecord::Base
 
   belongs_to :student
   has_many :resume_entries, through: :student
+  delegate :name, to: :student, allow_nil: true
 
   belongs_to :employer
+
+  def avatar_url
+    Gravatar.new(email).image_url(s: 100)
+  end
 end
