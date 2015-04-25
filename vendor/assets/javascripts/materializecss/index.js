198cde91f8017cccfb3ca6e374d1c69d130af225
0xazure <stuart.crust@gmail.com>
2015-04-25 15:57:35 -0400

    Update Materializecss to v0.96.1

--- vendor/assets/javascripts/materializecss/index.js
+++ vendor/assets/javascripts/materializecss/index.js
@@ -1,25 +1,32 @@
 //= require ./jquery.easing.1.3.js
-//= require ./animation.js
-//= require ./velocity.min.js
+//= require ./jquery.timeago.min.js
+
 //= require ./hammer.min.js
 //= require ./jquery.hammer.js
+
+//= require ./animation.js
+//= require ./buttons.js
+//= require ./cards.js
+//= require ./character_counter.js
 //= require ./collapsible.js
 //= require ./dropdown.js
+//= require ./forms.js
+//= require ./global.js
 //= require ./leanModal.js
 //= require ./materialbox.js
 //= require ./parallax.js
-//= require ./tabs.js
-//= require ./tooltip.js
-//= require ./waves.js
-//= require ./toasts.js
-//= require ./sideNav.js
-//= require ./scrollspy.js
-//= require ./forms.js
-//= require ./slider.js
-//= require ./cards.js
+//= require ./prism.js
 //= require ./pushpin.js
-//= require ./buttons.js
-//= require ./transitions.js
 //= require ./scrollFire.js
+//= require ./scrollspy.js
+//= require ./sideNav.js
+//= require ./slider.js
+//= require ./tabs.js
+//= require ./toasts.js
+//= require ./tooltip.js
+//= require ./transitions.js
+//= require ./velocity.min.js
+//= require ./waves.js
+
 //= require ./date_picker/picker.js
 //= require ./date_picker/picker.date.js
