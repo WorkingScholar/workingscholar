4c8dffaeb67293de5da6da1909ac3c28f2563a40
Sheret Ross <sheretr@gmail.com>
2015-04-27 19:26:59 -0400

    radiobox for posting

--- app/models/posting.rb
+++ app/models/posting.rb
@@ -1,5 +1,7 @@
 class Posting < ActiveRecord::Base
   belongs_to :employer
+  has_many :students
 
   validates :employer_id, presence: true
+  validates :compensation, inclusion: { in: %w(Pay Referral) }
 end
