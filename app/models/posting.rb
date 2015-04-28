8a2965f477033825c251318e72f0a8b9ae376779
Sheret Ross <sheretr@gmail.com>
2015-04-27 20:26:35 -0400

    adding validations to applications model

--- app/models/posting.rb
+++ app/models/posting.rb
@@ -1,7 +1,15 @@
 class Posting < ActiveRecord::Base
   belongs_to :employer
-  has_many :students
+  has_many :applications
 
   validates :employer_id, presence: true
-  validates :compensation, inclusion: { in: %w(Pay Referral) }
+  validates :compensation, inclusion: { in: ["Pay", "Referral"]}
+
+  def available?
+    if Application.find_by(posting_id: self.id, student_id: current_account.student.id)
+      # one exists
+    else
+      # does not exist
+    end
+  end
 end
