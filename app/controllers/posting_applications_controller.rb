efd99d3f71c092b471bae75cb967ca3a706a927c
Sheret Ross <sheretr@gmail.com>
2015-04-27 22:22:18 -0400

    applications model working

---
+++ app/controllers/posting_applications_controller.rb
@@ -0,0 +1,17 @@
+class PostingApplicationsController < ApplicationController
+  def create
+    @posting_application = current_account.student.posting_applications.build(posting_id: params[:posting_id])
+    if @posting_application.save
+      redirect_to profile_path(current_account.username),
+                  notice: "Successfully submitted resume."
+    else
+      render :new
+    end
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+end
