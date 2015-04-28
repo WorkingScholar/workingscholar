c1f9de99f2b13521e9e21265609aa00625ff0671
0xazure <stuart.crust@gmail.com>
2015-04-27 21:24:56 -0400

    Create Departments

--- app/models/student.rb
+++ app/models/student.rb
@@ -12,6 +12,9 @@ class Student < ActiveRecord::Base
   belongs_to :school, counter_cache: true
   delegate :name, to: :school, prefix: true, allow_nil: true
 
+  belongs_to :department, counter_cache: true
+  delegate :name, to: :department, prefix: true, allow_nil: true
+
   belongs_to :major, counter_cache: true
   delegate :name, to: :major, prefix: true, allow_nil: true
 
