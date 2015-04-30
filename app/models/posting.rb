8e994943e1e187b0c72ad74a027635ef8bbc66d6
Sheret Ross <sheretr@gmail.com>
2015-04-29 22:17:05 -0400

    number of applicants counter to employer profile posting view

--- app/models/posting.rb
+++ app/models/posting.rb
@@ -4,12 +4,4 @@ class Posting < ActiveRecord::Base
 
   validates :employer_id, presence: true
   validates :compensation, inclusion: { in: %w(Pay Referral) }
-
-  # def available?
-  #   if Application.find_by(posting_id: self.id, student_id: current_account.student.id)
-  #     # one exists
-  #   else
-  #     # does not exist
-  #   end
-  # end
 end
