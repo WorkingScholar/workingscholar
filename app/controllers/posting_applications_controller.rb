4ba54018b0054ae6cf7aa078e3579f2c03d6c3c0
Sheret Ross <sheretr@gmail.com>
2015-04-29 22:17:05 -0400

    number of applicants counter to employer profile posting view

--- app/controllers/posting_applications_controller.rb
+++ app/controllers/posting_applications_controller.rb
@@ -1,11 +1,12 @@
 class PostingApplicationsController < ApplicationController
   def create
-    @posting_application = current_account.student.posting_applications.build(posting_id: params[:posting_id])
+    @posting_application = current_account.student
+      .posting_applications.build(posting_id: params[:posting_id])
     if @posting_application.save
       redirect_to profile_path(current_account.username),
                   notice: "Successfully submitted resume."
     else
-      render :new
+      redirect_to postings_index_path
     end
   end
 
