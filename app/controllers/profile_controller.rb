3b67dab2883186ade3fafc898eb561de0f29fe00
0xazure <stuart.crust@gmail.com>
2015-04-30 19:53:48 -0400

    Fix #25
    Validate `:username` is alphanumeric + _.

--- app/controllers/profile_controller.rb
+++ app/controllers/profile_controller.rb
@@ -2,12 +2,12 @@ class ProfileController < ApplicationController
   before_action :set_profile, only: [:show]
 
   def root
-    redirect_to profile_path(current_account)
+    redirect_to profile_path(current_account.username)
   end
 
   private
 
     def set_profile
-      @account = Account.friendly.find(params[:id])
+      @account = Account.friendly.find(current_account.username)
     end
 end
